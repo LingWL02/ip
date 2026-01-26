64eb7712b637fae21efaaf0ef8416b6ae1903ab4
LingWL02 <154620085+LingWL02@users.noreply.github.com>
2026-01-27 01:39:06 +0800

    Added pattern matching skeleton.

--- src/main/java/Application.java
+++ src/main/java/Application.java
@@ -1,13 +1,19 @@
-import java.util.List;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 import java.util.Scanner;
 
 public class Application {
+    private static final PatternMapping[] patternMappings = {
+            new PatternMapping(Pattern.compile("^bye", Pattern.CASE_INSENSITIVE), ParsedType.TERMINATE)
+    };
+
     private final String name;
     private final String separator;
 
     private final Scanner appScanner = new Scanner(System.in);
-    private final List<String> toDoList = new ArrayList<String>();
+    private final List<String> taskList = new ArrayList<String>();
 
     public Application(String name, String separator) {
         this.name = name;
@@ -27,4 +33,38 @@ public class Application {
         System.out.printf("%s\n\nBye. Hope to see you again soon!\n%s", this.separator, this.separator);
     }
 
+    private ParsedResponse parseUserInput(String userInput) {
+        userInput = userInput.trim();  // Normalize string
+        return new ParsedResponse(ParsedType.TERMINATE, "");
+    }
+
 }
+
+enum ParsedType {
+    TERMINATE
+}
+
+class ParsedResponse {
+    private final ParsedType type;
+    private final String message;
+
+    public ParsedResponse(ParsedType type, String message) {
+        this.type = type;
+        this.message = message;
+    }
+
+    public ParsedType getType() {
+        return this.type;
+    }
+
+    public String getMessage() {
+        return this.message;
+    }
+}
+
+class PatternMapping extends Pair<Pattern, ParsedType> {
+    public PatternMapping(Pattern pattern, ParsedType parsedType) {
+        super(pattern, parsedType);
+    }
+}
+
