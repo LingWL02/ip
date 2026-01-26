c197fbce14a3f49e2c5cddb3fbdd3d20390ce16c
LingWL02 <154620085+LingWL02@users.noreply.github.com>
2026-01-26 23:49:38 +0800

    Level-1 Echo

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,3 +1,7 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class Duke {
     public static void main(String[] args) {
         String logo = " ____        _        \n"
@@ -6,16 +10,21 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
         String separator = "_".repeat(50);
 
-        String introduction = (
-                separator +
-                "\nHello! I'm Duke! What can I do for you?\n"
-                + separator +
-                "\nBye. Hope to see you again soon!\n" +
-                separator
-        );
-        System.out.println(introduction);
+        System.out.println(separator + "\nHello! I'm Duke!\nWhat can I do for you?\n"+ separator + "\n");
 
+        Scanner myScanner = new Scanner(System.in);
+        Pattern endPattern = Pattern.compile("bye", Pattern.CASE_INSENSITIVE);
+
+        while(true) {
+            String userInput = myScanner.nextLine();
+            if (endPattern.matcher(userInput).find()) break;
+
+            System.out.println(userInput + "\n" + separator + "\n");
+        }
+
+        System.out.println(separator + "\nBye. Hope to see you again soon!\n" + separator);
     }
 }
